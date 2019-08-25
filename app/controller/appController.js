05912e4c70ca64287e4783669eea659fac2923da
hison731 <jinxing4@outlook.com>
2019-08-26 06:40:48 +0800

    Controller Init

---
+++ app/controller/appController.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var User = require('../model/appModel.js');
+
+exports.read_a_user = function(req, res) {
+
+  var userId = req.body.id;
+
+  User.getUserById(userId, function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
+
+exports.list_all_users = function(req, res) {
+
+  User.getUserList(function(err, user) {
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
+};
