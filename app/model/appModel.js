35c0b5b60f418c403c17c258a7e2e07e2544aadc
hison731 <jinxing4@outlook.com>
2019-08-26 06:41:02 +0800

    Model Init

---
+++ app/model/appModel.js
@@ -0,0 +1,31 @@
+'user strict';
+var sql = require('./db.js');
+
+var User = function(){
+};
+
+User.getUserById = function (userId, result) {
+    sql.query("Select * from users where id = ? ", userId, function (err, res) {             
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            }
+            else{
+                result(null, res);
+            }
+        });   
+};
+
+User.getUserList = function (result) {
+    sql.query("Select * from users", function (err, res) {             
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            }
+            else{
+                result(null, res);
+            }
+        });   
+};
+
+module.exports= User;
